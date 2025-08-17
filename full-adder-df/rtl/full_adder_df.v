9b69e09cc2bb7e9097b2ac096d9ece1adb09c2c2
Hithaishi S R <134951717+Hithaishisr@users.noreply.github.com>
2025-08-17 16:59:35 +0530

    Add files via upload

---
+++ full-adder-df/rtl/full_adder_df.v
@@ -0,0 +1,15 @@
+module full_adder_df (
+    a_in,
+    b_in,
+    c_in,
+    sum_out,
+    carry_out
+);
+
+  input a_in, b_in, c_in;
+  output sum_out, carry_out;
+
+  assign sum_out   = a_in ^ b_in ^ c_in;
+  assign carry_out = a_in & b_in | c_in & (a_in ^ b_in);
+
+endmodule
